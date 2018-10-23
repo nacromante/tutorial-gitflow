bde609b5c3c52f7d870aa56f69603f7439a7b35a
Frederico Hadar Correia Queiroz <frederico.queiroz@hsd.int>
2018-10-23 14:18:35 -0300

    commit inicial

---
+++ src/main/java/br/com/fred/tutorialgitflow/TutorialGitflowApplication.java
@@ -0,0 +1,12 @@
+package br.com.fred.tutorialgitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialGitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialGitflowApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/fred/tutorialgitflow/TutorialGitflowApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fred.tutorialgitflow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TutorialGitflowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
